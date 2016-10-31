18b0ad0c7986cc4da08e2cbd25beb1e793755ad8
VictoriaOtm <victoriaotmahova@gmail.com>
2016-10-31 22:29:18 +0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,49 +1,82 @@
 #Классы будут в этом файле
-
 import blowfish
 from os import urandom
 
 
 class Account:
-
-    def __init__(self, login, password):
-        self.__login = login.encode()
-        self.__password = password.encode()
-        self.cipher = blowfish.Cipher(b'#dskfdfhg#dflg3642/e=wjh+dflwqo22de94hiiwehbsjwk')
-        self.iv = urandom(8)
-
-
-    def returnAccount(self):
-        return [self.__login, self.__password]
-
+    """
+    __login -> contains users login
+    __password -> contains users password
+    __cipher -> contains cipher
+    __iv -> ??? needed for blowfish
+    """
 
     def encrypt(self):
-        login_encrypted = b"".join(self.cipher.encrypt_cfb(self.__login, self.iv))
-        pass_encrypted = b"".join(self.cipher.encrypt_cfb(self.__password, self.iv))
-        return [login_encrypted, pass_encrypted]
+        if self.__login is not None:
+            self.__login = b"".join(self.__cipher.encrypt_cfb(self.__login, self.__iv))
+        if self.__password is not None:
+            self.__password = b"".join(self.__cipher.encrypt_cfb(self.__password, self.__iv))
 
+    def __init__(self, login="", password=""):
+        if login is not None:
+            self.__login = login.encode()
+        else:
+            self.__login = None
 
-    def decrypt(self, account_encrypted):
-        login_decrypted = b"".join(self.cipher.decrypt_cfb(account_encrypted[0], self.iv))
-        pass_decrypted = b"".join(self.cipher.decrypt_cfb(account_encrypted[1], self.iv))
+        if password is not None:
+            self.__password = password.encode()
+        else:
+            self.__password = None
+
+        self.__cipher = blowfish.Cipher(b'#dskfdfhg#dflg3642/e=wjh+dflwqo22de94hiiwehbsjwk')
+        self.__iv = urandom(8)
+        self.encrypt()
+
+    def return_account(self):
+        if self.__login is not None:
+            login_decrypted = b"".join(self.__cipher.decrypt_cfb(self.__login, self.__iv))
+        else:
+            login_decrypted = b''
+        if self.__password is not None:
+            pass_decrypted = b"".join(self.__cipher.decrypt_cfb(self.__password, self.__iv))
+        else:
+            pass_decrypted = b''
         return [login_decrypted.decode(), pass_decrypted.decode()]
 
 
 class User:
+    """
+    __accounts -> not really needed dict. Just for now
+    __userId -> user id
+    __firstName ->
+    __lastName ->
+    """
     __accounts = {}
 
-    def __init__(self, first_name, last_name, id):
-        self.__userId = id
-        self.__first_name = first_name
-        self.__last_name = last_name
+    def __init__(self, first_name=None, last_name=None, user_id=None):
+        self.__userId = user_id
+        self.__firstName = first_name
+        self.__lastName = last_name
 
     def name(self):
-        return [self.__first_name, self.__last_name]
+        return [self.__firstName, self.__lastName]
 
-    def addAccount(self, site, acc):
-        if((self.__accounts).get(site) == None):
+    def add_account(self, site, acc):
+        if self.__accounts.get(site) is None:
             self.__accounts[site] = acc
+            return True
+        else:
+            return False
 
-    def returnAccount(self, site):
+    def return_sites(self):
+        return self.__accounts.keys()
+
+    def return_account(self, site):
         temp = self.__accounts.get(site)
-        return [site, temp.returnAccount()]
+        if temp is not None:
+            return [site, temp.return_account()]
+        else:
+            return None
+
+    def return_id(self):
+        return self.__userId
