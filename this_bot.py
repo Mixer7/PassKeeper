fea65add93b27139d133fcd56bb4e074b80f01f4
VictoriaOtm <victoriaotmahova@gmail.com>
2016-10-31 22:30:45 +0300

    Update this_bot.py

--- this_bot.py
+++ this_bot.py
@@ -1,9 +1,9 @@
-#это только стартовый код, на нем тестировали разные штуки
 
 import telebot
 from classes import User
 from classes import Account
 
+testUser = User('name', 'surname', '123123123123')
 token = '292439910:AAHd72nbGZz7dfsySHvZpb4-ih2KrGxYvJU'
 
 bot = telebot.TeleBot(token)
@@ -20,14 +20,53 @@ def get_obj(num_of_obj, obj_list):
 def create_user(message):
 
     this_user = User(message.from_user.first_name, message.from_user.last_name, message.from_user.id)
-    bot.send_message(message.chat.id, 'Привет!, {}'.format(message.from_user.first_name))
+    bot.send_message(message.chat.id, 'Привет, {}!'.format(message.from_user.first_name))
 
 
 @bot.message_handler(commands=['help'])
 def list_commands(message):
+    bot.send_message(message.chat.id, '/start - новый пользователь\n'
+                                      '/new - запись нового аккаунта в базу\n'
+                                      '/tellaccount - просмотр логина и пароля\n'
+                                      '/sites - вывод сайтов в базе данных')
 
-    bot.send_message(message.chat.id, '/start - новый пользователь\n/new - запись нового аккаунта в базу\n/tellaccount - просмотр логина и пароля')
 
+@bot.message_handler(commands=['new'])
+def new_acc(message):
+    mytext = (str(message.text)).split()
+    if len(mytext) == 4:
+        testUser.add_account(mytext[1], Account(mytext[2], mytext[3]))
+        bot.send_message(message.chat.id, "Oke")
+    else:
+        bot.send_message(message.chat.id, "Not oke")
+
+
+@bot.message_handler(commands=['sites'])
+def give_sites(message):
+    temp = "\n".join(testUser.return_sites())
+    if temp != '':
+        bot.send_message(message.chat.id, temp)
+    else:
+        bot.send_message(message.chat.id, "Not oke")
+
+@bot.message_handler(commands=['tellaccount'])
+def give_acc(message):
+    mytext = (str(message.text)).split()
+    if len(mytext) == 2:
+        myacc = testUser.return_account(mytext[1])
+        if myacc is None:
+            bot.send_message(message.chat.id, "Not oke")
+            return
+        if myacc[0] is None:
+            myacc[0] = ''
+        if myacc[1][0] is None:
+            myacc[1][0] = ''
+        if myacc[1][1] is None:
+            myacc[1][1] = ''
+
+        bot.send_message(message.chat.id, "Site: " + myacc[0] + " Login: " + myacc[1][0] + " Password: " + myacc[1][1])
+    else:
+        bot.send_message(message.chat.id, "Not oke")
 
 
 @bot.message_handler(content_types = ['text'])
@@ -49,5 +88,7 @@ def remember_acc(message):
         bot.send_message(message.chat.id, 'Простите, я вас не понимаю')
 
 
+
 if __name__ == "__main__":
+
     bot.polling(none_stop=True)
